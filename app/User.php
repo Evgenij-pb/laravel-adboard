e090f96e20e78e7aa67293f73f813c88444ba9cc
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-19 13:04:16 +0200

    add Ad and Category models with relations

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'phone',
     ];
 
     /**
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function ads(){
+        return $this->hasMany(Task::class);
+    }
 }
