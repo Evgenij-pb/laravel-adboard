cc90f5bb073d8b62ef9b3750b9dabf26000d1903
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-19 12:41:44 +0200

    make migrations

---
+++ database/migrations/2022_11_18_185547_create_ads_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->string('title');
+            $table->string('image');
+            $table->text('description');
+            $table->timestamp('expires_at');
+            $table->boolean('is_verified');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ads');
+    }
+}
