a60617d3c33f3c3e157805faac1005d255b6b2e9
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-12-12 22:52:53 +0200

    add adminCheck middleware

---
+++ app/Http/Middleware/AdminCheck.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest('login');
+            }
+        }else{
+            $user = Auth::user();
+            if (($user->name !== 'Admin')&&($user->id!=='1')){
+                abort(403, 'You are not moderator');
+            }
+        }
+
+        return $next($request);
+    }
+}
